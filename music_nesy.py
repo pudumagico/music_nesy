78938095c0136914bff47242e1f0bcd9003260c1
pudumagico <nhigueraruiz@gmail.com>
2021-06-10 12:43:18 +0200

    ready to show

--- music_nesy.py
+++ music_nesy.py
@@ -3,66 +3,81 @@ import numpy as np
 import clingo
 from midiutil import MIDIFile
 from note2midi import note_to_number
-# proc = madmom.features.chords.CNNChordFeatureProcessor()
-# feats = proc('tests/Backing Track C - Am - Dm - G (S.Soul) tempo 80.mp3')
-# decode = madmom.features.chords.CRFChordRecognitionProcessor()
-#
-# decoded_feats = decode(feats).tolist()
-#
-# # select most common chords in the track
-# chord_dict = {}
-# for chord in decoded_feats:
-#     if chord[2] not in chord_dict:
-#         chord_dict[chord[2]] = chord[1] - chord[0]
-#     else:
-#         chord_dict[chord[2]] += chord[1] - chord[0]
-#
-# print(chord_dict)
-#
-# # simple criteria for this would be above 20 seconds
-# treshold = 20
-# chord_list = []
-# for chord in chord_dict:
-#     if chord_dict[chord] > treshold:
-#         chord_list.append(chord)
-#
-# # a little bit more of parsing
-# parsed_chords = []
-# for chord in chord_list:
-#     split = chord.lower().split(':')
-#     parsed_chords.append((split[0], split[1]))
-#
-# print(parsed_chords)
-parsed_chords = [('c', 'maj'), ('a', 'min'), ('g', 'maj'), ('d', 'min')]
+
+filename = 'Backing Track C - Am - Dm - G (S.Soul) tempo 80.mp3'
+
+proc = madmom.features.chords.CNNChordFeatureProcessor()
+feats = proc('tracks/' + filename)
+decode = madmom.features.chords.CRFChordRecognitionProcessor()
+
+decoded_feats = decode(feats).tolist()
+
+print('decoded features')
+print(decoded_feats)
+
+# select most common chords in the track
+chord_dict = {}
+total_length = 0
+for chord in decoded_feats:
+    if chord[2] not in chord_dict:
+        chord_dict[chord[2]] = chord[1] - chord[0]
+        total_length += chord[1] - chord[0]
+    else:
+        chord_dict[chord[2]] += chord[1] - chord[0]
+        total_length += chord[1] - chord[0]
+
+print('chord dict')
+print(chord_dict)
+
+print(total_length)
+
+treshold = total_length * 0.2
+chord_list = []
+for chord in chord_dict:
+    if chord_dict[chord] > treshold:
+        chord_list.append(chord)
+
+print('chord list')
+print(chord_list)
+
+parsed_chords = []
+for chord in chord_list:
+    split = chord.lower().split(':')
+    parsed_chords.append((split[0], split[1]))
+
+print('parsed chords')
+print(parsed_chords)
+
 ctl = clingo.Control()
 
 ctl.load('chords.lp')
 ctl.load('scales.lp')
 ctl.load('melody.lp')
 ctl.add('base', [], ' '.join(f'chord({t},{s}).' for t,s in parsed_chords))
-ctl.add('base', [], '#show melody/2.')
+ctl.add('base', [], '#show melody/2. #show scale/2. #show note/1.')
 
 ctl.ground(parts=[('base', [])])
 solvehandle = ctl.solve(yield_=True)
-
-model = solvehandle.model()
+# print(solvehandle.__dict__)
+for m in solvehandle:
+    model = m
+# model = solvehandle.model()
 
 melody = []
 for symbol in model.symbols(shown=True):
-    # print(symbol, str(symbol.arguments[0]), str(symbol.arguments[1]))
-    melody.append((str(symbol.arguments[0]), int(str(symbol.arguments[1]))))
+    print(symbol)
+    if 'melody' in str(symbol):
+        melody.append((str(symbol.arguments[0]), int(str(symbol.arguments[1]))))
 
 melody.sort(key=lambda x:x[1])
-print(melody)
-
 
 parsed_melody = []
 for note in melody:
     parsed_melody.append(note_to_number(note[0], 6))
 
+print('parsed melody')
 print(parsed_melody)
-#
-# degrees  = [60, 62, 64, 65, 67, 69, 71, 72]  # MIDI note number
+
 track    = 0
 channel  = 0
 time     = 0    # In beats
@@ -77,5 +92,5 @@ MyMIDI.addTempo(track, time, tempo)
 for i, pitch in enumerate(parsed_melody):
     MyMIDI.addNote(track, channel, pitch, time + i, duration, volume)
 
-with open("major-scale.mid", "wb") as output_file:
+with open(filename + '.mid', "wb") as output_file:
     MyMIDI.writeFile(output_file)
